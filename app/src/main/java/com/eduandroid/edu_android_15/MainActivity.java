a5f740494a3bda82b13fa15481e580d31ce46dc7
Ness <ness@MacBook-Pro-Ness.local>
2023-07-02 17:17:18 +0300

    first commit

---
+++ app/src/main/java/com/eduandroid/edu_android_15/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eduandroid.edu_android_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
